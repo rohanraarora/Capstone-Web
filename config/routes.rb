a0c3eb84495f68bd2aaaa970e86c14f6e78e7736
Rohan Arora <rohanraarora@gmail.com>
2016-10-19 21:10:50 +0530

    Add Speaker Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :speakers
+  end
   resources :news
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
